513db199e4790d60286894196f406b95e8798c43
Lucas Marques <luandrealbuquerque@gmail.com>
2025-04-06 16:00:21 -0300

    Implement binary search algorithm in a new file

---
+++ binary-search.py
@@ -0,0 +1,20 @@
+arr = [1,2,3,4,5]
+target = 4
+
+def binary_search(nums, target):
+  left = 0
+  right = len(nums) - 1
+
+  while left <= right:
+    mid = (left + right) // 2
+
+    if nums[mid] == target:
+      return mid
+    if nums[mid] < target:
+      left = mid + 1
+    else:
+      right = mid - 1
+
+  return -1
+
+print(binary_search(arr, target))
